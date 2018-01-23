f89f7580bdeebf462ff297348c2a38a441aa224d
ArtCouSan <artcousan@gmail.com>
2018-01-23 21:31:50 -0200

    Aleatorios funcionando 100%

--- appTrocadilhos/src/pages/aleatorios/aleatorios.ts
+++ appTrocadilhos/src/pages/aleatorios/aleatorios.ts
@@ -4,6 +4,8 @@ import { NavController } from 'ionic-angular';
 import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database-deprecated';
 import { TrocadilhoItem } from '../../models/trocadilho-item';
 import { NavParams } from 'ionic-angular/navigation/nav-params';
+declare function require(path: string) : any;
+var shuffle = require('shuffle-array');
 
 @Component({
   selector: 'page-aleatorios',
@@ -22,7 +24,9 @@ export class AleatorioPage {
       item.forEach(_item => {
        this.items.push(_item);
       });
+      shuffle(this.items);
     });
+
 
   }
 
